bea8ef3a188a4c96ed801f22687971e712e40e42
Anthony Ugwu <anthonyugwu@MacBook-Pro-72.local>
2018-09-25 18:31:10 +0100

    chore(integrate databse)setup postgres database
    - remove mongodb and setup postgres using sequelize
    
     [Finishes #160618090]

---
+++ models/article.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Article = sequelize.define('Article', {
+    title: DataTypes.STRING
+  }, {});
+  Article.associate = function(models) {
+    // associations can be defined here
+  };
+  return Article;
+};
