948e5360fd029ab516011e9503d2bb68fc3aa2a8
Anthony Ugwu <anthonyugwu@MacBook-Pro-72.local>
2018-09-27 07:23:17 +0100

    chore(setup documention)setup documentaion using swagger
    [finishes #160618096]

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const db =require('./models');
+const db = require("./models/index");
 
 const app = express();
+
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(morgan("dev"));
 
@@ -14,21 +18,19 @@ app.get("/", (req, res) => res.send({ message: "Welcome to Author's Haven" }));
 
 //test  database connection
 
-app.get("/article",async (req,res)=>{
-    try {
-        const article = await db.Article.findAll({})
-        return res.status(400).json({
-            status:"Success",
-            data:article
-        })
-    } catch (error) {
-        return res.json({
-            status:"error",
-        })
-    }
-
-})
-
+app.get("/article", async (req, res) => {
+  try {
+    const article = await db.Article.findAll({});
+    return res.status(400).json({
+      status: "Success",
+      data: article
+    });
+  } catch (error) {
+    return res.json({
+      status: "error"
+    });
+  }
+});
 
 const port = process.env.PORT || 3200;
 
