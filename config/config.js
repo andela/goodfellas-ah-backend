d8e822c03601f762216569b9737711b9e2e3aecb
phunmbi <phunmbi@gmail.com>
2018-09-27 14:59:20 +0100

    chore(test): Setup test db

--- config/config.js
+++ config/config.js
@@ -9,10 +9,10 @@ module.exports = {
     dialect: "postgres"
   },
   test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.TEST_DATABASE,
+    host: process.env.DB_HOST,
     dialect: "postgres"
   },
   production: {
