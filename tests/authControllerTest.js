2699f5a7c34330a1c8f870049dba1e9a9987a618
AdinoyiSadiq <sadiqadinoyi@gmail.com>
2018-10-03 04:35:55 +0100

    feat(test): add failing signin test
    [Delivers #160618097]

--- tests/authControllerTest.js
+++ tests/authControllerTest.js
@@ -132,5 +132,82 @@ describe('Authentication controller', () => {
           done();
         });
     });
+
+    it('Should return an error message when the user tries to sign in with an incorrect email', done => {
+      chai
+        .request(app)
+        .post('/api/auth/signin')
+        .send({
+          "email": "wrongemail@gmail.com",
+          "password": "myPassword"
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+          expect(res.body.message).to.equal('The account with this email does not exist');
+          done();
+        });
+    });
+
+    it('Should return an error message when the user tries to sign in with an incorrect password', done => {
+      chai
+        .request(app)
+        .post('/api/auth/signin')
+        .send({
+          "email": "sadiqadinoyi@gmail.com",
+          "password": "wrongPassword"
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+          expect(res.body.message).to.equal('Incorrect email or password');
+          done();
+        });
+    });
+
+    it('Should return an error message when the user tries to sign in with no data', done => {
+      chai  
+        .request(app)
+        .post('/api/auth/signin')
+        .send({
+          "email": "",
+          "password": ""
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+          expect(res.body.message).to.equal('Please fill the email and password fields');
+          done();
+        });
+    });
+
+    it('Should return an error message when the user tries to sign in with a missing field', done => {
+      chai  
+        .request(app)
+        .post('/api/auth/signin')
+        .send({
+          "email": "myemail@gmail.com",
+          "password": ""
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+          expect(res.body.message).to.equal('Please fill the password field');
+          done();
+        });
+    });
+
+    it('Should return an error message when the user tries to sign in after passing more than the required fields', done => {
+      chai
+        .request(app)
+        .post('/api/auth/signup')
+        .send({
+          "username": "Sadiq",
+          "email": "myemail@gmail.com",
+          "password": "password",
+          "occupation": "Software developer"
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+          expect(res.body.message).to.equal('Too many fields');
+          done();
+        });
+    });
   });
 });
