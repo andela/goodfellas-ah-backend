e3ba401293583c279ea63332d7a0a690c3247160
phunmbi <phunmbi@gmail.com>
2018-10-04 17:39:09 +0100

    feature(googleLogin): Configure google authentication strategy

---
+++ config/passport.js
@@ -0,0 +1,20 @@
+const passport = require('passport');
+const GooglePlusTokenStrategy = require('passport-google-plus-token');
+
+// Configure the Google passport strategy
+const googleStrategy = passport.use(new GooglePlusTokenStrategy({
+  clientID: '394162324941-1naldjcsruq9mmfojg3qipbm8c72mdvl.apps.googleusercontent.com',
+  clientSecret: 'yXLRkfOwg9SI-Gg0eXO46GuN'
+}, ((accessToken, refreshToken, profile, done) => {
+  // Get access to the user details and send it to the controller
+  const user = {
+    firstName: profile.name.givenName,
+    lastName: profile.name.familyName,
+    email: profile.emails[0].value,
+    password: profile.id
+  };
+
+  done(null, user);
+})));
+
+module.exports = googleStrategy;
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
+const passport = require('passport');
 
 const userController = require('../../controllers/userController');
 const { validate } = require('../..//middleware/validation');
+const googleToken = require('../../config/passport');
 
 router.post('/auth/signup', validate('signup'), userController.signup);
 router.post('/auth/signin', validate('signin'), userController.signin);
+router.post('/auth/googlesignup', passport.authenticate('google-plus-token', { session: false }));
 
 module.exports = router;
