a6bfd9687bb30af93ee3ac1626371fe0fc3b885b
damafeez <damafeez@gmail.com>
2018-10-08 17:26:15 +0100

    chore(validator): implement validator
     - add test route to visualize validator
     - add testRules to utility
    
    Finished  #161051470

---
+++ middleware/validator.js
@@ -0,0 +1,48 @@
+const convertArgsToArray = mixedArgs => mixedArgs
+  .map((eachArg) => {
+    if (typeof eachArg === 'object') {
+      const objectKeys = Object.keys(eachArg);
+      return [objectKeys[0], eachArg[objectKeys[0]]];
+    }
+    return [eachArg];
+  });
+const validator = {
+  required(inputField, field) {
+    return Boolean(inputField) || `${field} is required`;
+  },
+  minLength(inputField, field, min) {
+    return inputField && inputField.length >= min ? true : `${field} should have minimum of ${min} characters`;
+  },
+  dataType(inputField, field, type) {
+    if (inputField && type === 'email') {
+      return /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(inputField) || `${field} should be of type ${type}`;
+    }
+    return typeof inputField === type || `${field} should be of type ${type}`;
+  },
+  validate(validationRules, userInput) {
+    const error = Object.keys(validationRules)
+      .map((field) => {
+        if (validationRules[field].trim && typeof userInput[field] === 'string') userInput[field] = userInput[field].trim();
+        return convertArgsToArray(validationRules[field].rules)
+          .map(rule => this[rule[0]](
+            userInput[field],
+            field,
+            rule[1],
+          ));
+      })
+      .reduce((accumulator, current) => [...accumulator, ...current], [])
+      .filter(test => test !== true);
+    return error;
+  }
+};
+
+export default validationRules => (req, res, next) => {
+  const error = validator.validate(validationRules, req.body);
+  if (error.length > 0) {
+    return res.status(400).json({
+      data: {},
+      message: error,
+    });
+  }
+  return next();
+};
--- routes/api/user.js
+++ routes/api/user.js
@@ -1,13 +1,18 @@
 import express from 'express';
 import authenticate from '../../middleware/authentication';
+import validator from '../../middleware/validator';
+import { testRules } from '../../lib/utility';
 
 const router = express.Router();
-
 const userController = require('../../controllers/userController');
 
 router.post('/user/follow', authenticate, userController.follow);
 router.post('/user/unfollow', authenticate, userController.unfollow);
 router.get('/user/followed', authenticate, userController.followed);
 router.get('/user/followers', authenticate, userController.followers);
+router.post('/test/validator', validator(testRules), (req, res) => res.send({
+  message: 'You got here, you are so cool.',
+  data: req.body
+}));
 
 module.exports = router;
