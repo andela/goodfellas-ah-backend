f31ca4709afc677dee60689cc97d87b6731475a5
damafeez <damafeez@gmail.com>
2018-10-16 10:29:05 +0100

    feature(authentication): add allowVisitors middleware

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -1,5 +1,5 @@
 import articleController from '../../controllers/articleController';
-import authenticate from '../../middleware/authentication';
+import authenticate, { allowVisitors } from '../../middleware/authentication';
 import { checkNullInput, reactionValidation } from '../../middleware/validation';
 
 const router = require('express').Router();
@@ -7,8 +7,8 @@ const router = require('express').Router();
 router.post('/articles', authenticate, checkNullInput, articleController.createArticle);
 router.put('/articles/:slug', authenticate, checkNullInput, articleController.updateArticle);
 router.delete('/articles/:slug', authenticate, articleController.deleteArticle);
-router.get('/articles', authenticate, articleController.getAllArticles);
-router.get('/articles/:slug', authenticate, articleController.getAnArticle);
+router.get('/articles', allowVisitors, articleController.getAllArticles);
+router.get('/articles/:slug', allowVisitors, articleController.getAnArticle);
 router.post('/articles/:slug/react', authenticate, reactionValidation, articleController.reactToArticle);
 router.post('/articles/:slug/bookmark', authenticate, articleController.bookmarkArticle);
 router.delete('/articles/:slug/bookmark', authenticate, articleController.deleteBookmark);
